8a271892ef2935ac7430cae03c5e79dc8103ed53
DANIZIN <98532797+DANIZIN23@users.noreply.github.com>
2022-10-19 22:43:50 -0300

    Update AtlasFrameMaker.hx

--- source/animateatlas/AtlasFrameMaker.hx
+++ source/animateatlas/AtlasFrameMaker.hx
@@ -16,10 +16,9 @@ import flixel.graphics.frames.FlxFrame;
 #if desktop
 import sys.FileSystem;
 import sys.io.File;
-#else
-import js.html.FileSystem;
-import js.html.File;
 #end
+
+
 
 using StringTools;
 class AtlasFrameMaker extends FlxFramesCollection
